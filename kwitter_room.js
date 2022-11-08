7e75c131ddb2b31195b91dd09705309ac1a1b614
Mastercoder-Divit <98080914+Mastercoder-Divit@users.noreply.github.com>
2022-11-08 16:39:27 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC_za1Hx5D8j48LPM-NlgkuAkQSYDR4lrk",
+    authDomain: "kwitter-project-5a458.firebaseapp.com",
+    databaseURL: "https://kwitter-project-5a458-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-5a458",
+    storageBucket: "kwitter-project-5a458.appspot.com",
+    messagingSenderId: "292061934776",
+    appId: "1:292061934776:web:4e91e191c820e72f98b04d",
+    measurementId: "G-F74QQTHVNS"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+    Room_names = childKey;
+   //Start code
+
+   //End code
+   });});}
+getData();
